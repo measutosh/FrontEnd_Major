11c89e10beac5a42d6390e33ff42540da84d0254
meAsutosh <78539272+meAsutosh@users.noreply.github.com>
2022-01-29 11:30:59 +0530

    added music-player app

---
+++ music-player/js/music-list.js
@@ -0,0 +1,22 @@
+
+let allMusic = [
+  {
+    name: "Kebe Aasi Tu",
+    artist: "Kuldeep & Aseema",
+    img: "Kebe Aasi Tu",
+    src: "Kebe Aasi Tu"
+  },
+  {
+    name: "Baramasi",
+    artist: "Humane & Jyotirmayee",
+    img: "Baramasi",
+    src: "Baramasi"
+  },
+  {
+    name: "Urvashi",
+    artist: "Abhinash",
+    img: "Urbashi",
+    src: "Urbashi"
+  }
+];
+
